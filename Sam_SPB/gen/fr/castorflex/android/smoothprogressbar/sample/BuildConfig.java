a6c563ec0e6cb62576b03b947ead5e73930b5b8e
hinyon <hinyon@SevenSquare>
2013-12-23 14:51:19 +0800

    开始整合常用框架包

---
+++ Sam_SPB/gen/fr/castorflex/android/smoothprogressbar/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package fr.castorflex.android.smoothprogressbar.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
